ebe8fb12682fb79aa2069b9951c8f14330cab165
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-02-11 17:25:15 +0000

    Refactored our power saving modes & added a reset function

--- spec/thermostat_spec.js
+++ spec/thermostat_spec.js
@@ -8,17 +8,17 @@ describe('Thermostat', function() {
     });
 
     it('starts at 20 degrees', function(){
-        expect(thermostat.temperature()).toEqual (20);
+        expect(thermostat.checkTemp()).toEqual (20);
     });
 
     it('increases the temperature', function(){
         expect(thermostat.raiseTemp())
-        expect(thermostat.temperature()).toEqual (21);
+        expect(thermostat.checkTemp()).toEqual (21);
     });
 
     it('decreases the temperature', function(){
         expect(thermostat.decreaseTemp())
-        expect(thermostat.temperature()).toEqual (19);
+        expect(thermostat.checkTemp()).toEqual (19);
     });
 
     it('has a minimum of 10 degrees', function(){
@@ -29,27 +29,45 @@ describe('Thermostat', function() {
         for (var i = 1; i < 11; i++) {
             thermostat.decreaseTemp();
         } 
-        expect(thermostat.temperature()).toEqual (10)
+        expect(thermostat.checkTemp()).toEqual (10)
         expect(function(){
         thermostat.decreaseTemp();
         }).toThrowError('Minimum temperature reached');
     });
 
     it('has a power saving mode', function(){
-    expect(thermostat.powerMode()).toEqual (true);
+    expect(thermostat.checkPowerMode()).toEqual (true);
     });
 
     it('the maximum temperature is 25 degrees if power saving mode is on', function(){
-        expect(thermostat.powerMode()).toEqual (true);
+        expect(thermostat.checkPowerMode()).toEqual (true);
         for (var j = 0; j < 5; j++) {
             thermostat.raiseTemp();
         }
-        expect(thermostat.temperature()).toEqual (25)
+        expect(thermostat.checkTemp()).toEqual(25)
         expect(function(){
         thermostat.raiseTemp();
         }).toThrowError('maximum temperature reached');
         
-       
+    });
+
+    it('the maximum temperature is 32 degrees if power saving mode is off', function(){
+        thermostat.powerModeOff()
+        expect(thermostat.checkPowerMode()).toEqual(false);
+        for (var j = 0; j < 12; j++) {
+            thermostat.raiseTemp();
+        }
+        expect(thermostat.checkTemp()).toEqual(32)
+        expect(function(){
+        thermostat.raiseTemp();
+        }).toThrowError('maximum temperature reached');
+    });
+
+    it('can reset the temperature to a default of 20 degrees', function(){
+        thermostat.raiseTemp()
+        expect(thermostat.checkTemp()).toEqual(21);
+        thermostat.resetTemp()
+        expect(thermostat.checkTemp()).toEqual(20);
     });
 
 }); 
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,40 +2,57 @@
 
 class Thermostat{
     constructor() { 
-        this.defaultTemperature = 20;
+        this.DEFAULT_TEMPERATURE = 20;
+        this.currentTemperature = this.DEFAULT_TEMPERATURE;
         this.MINIMUM_TEMPERATURE = 10;
         this.powerSavingMode = true;
-        this.maximumTemperature = 25;
+        this.MAXIMUM_TEMP_PSM_ON = 25;
+        this.MAXIMUM_TEMP_PSM_OFF = 32;
     };
     
-    temperature(){
-        return this.defaultTemperature;
+    checkTemp(){
+        return this.currentTemperature;
     };
 
+    resetTemp(){
+        this.currentTemperature = this.DEFAULT_TEMPERATURE;
+    }
+
     raiseTemp(){
-        if (this.maximumTemp()) {
+        if (this.isMaximumTemp()) {
             throw new Error('maximum temperature reached');
         }
-     this.defaultTemperature += 1;
+     this.currentTemperature += 1;
     }
 
     decreaseTemp(){
-       if (this.minimumTemp()) {
+       if (this.isMinimumTemp()) {
            throw new Error('Minimum temperature reached');
        }
-       this.defaultTemperature -= 1;
+       this.currentTemperature -= 1;
     }
 
-    minimumTemp(){
-        return this.defaultTemperature === this.MINIMUM_TEMPERATURE;
-    };
-
-    powerMode(){
+    checkPowerMode(){
         return this.powerSavingMode;
     };
 
-    maximumTemp() {
-        return this.defaultTemperature === this.maximumTemperature;
+    powerModeOff(){
+        this.powerSavingMode = false;
     }
+
+    powerModeOn(){
+        this.powerSavingMode = true;
+    }
+
+    isMaximumTemp() {
+        if (this.checkPowerMode() === false) { 
+            return this.currentTemperature === this.MAXIMUM_TEMP_PSM_OFF;
+        }
+        return this.currentTemperature === this.MAXIMUM_TEMP_PSM_ON;
+    }
+
+    isMinimumTemp(){
+        return this.currentTemperature === this.MINIMUM_TEMPERATURE;
+    };
 };
 
